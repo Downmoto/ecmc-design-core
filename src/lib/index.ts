788961d3187957ed0fb328f5208791c35ad3eeb6
Arad Fadaei <arad@fadaei.dev>
2025-12-20 20:47:33 -0500

    fix: remove redundant export statement

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -10,6 +10,3 @@ export { default as Centered } from './atoms/container/sub/centered.container.sv
 
 export { default as Text } from './atoms/text/text.svelte';
 export type { TextProps } from './atoms/text/text.svelte.ts';
-
-// Reexport utilities
-export { cn } from './utils/cn.js';
